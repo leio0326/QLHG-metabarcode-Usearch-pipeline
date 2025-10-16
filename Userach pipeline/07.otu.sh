4badeb04c4799dffbb84fa9a3119d287713e10f9
leio0326 <136607704+leio0326@users.noreply.github.com>
2025-10-16 15:15:10 +0800

    Update 07.otu.sh

--- Userach pipeline/07.otu.sh
+++ Userach pipeline/07.otu.sh
@@ -1,5 +1,9 @@
-cd /data01/duanlei/ecology/rawdata/COI/20250115
-
+#!/bin/bash
+cd "your working path"
+USEARCH_PATH="~/soft/usearch"
+INPUT_DIR="02.Cutprimer"
+OUTPUT_DIR="03.Merged"
+mkdir -p "$OUTPUT_DIR"
 usearch -sortbysize 06.Unique/all.uni.fa -fastaout 06.Unique/all.sort.fa -minsize 2
 usearch -cluster_otus 06.Unique/all.sort.fa -otus 07.OTU/cluster/all.otus.fa -relabel OTU -uparseout 07.OTU/cluster/all_out -minsize 5
 usearch -unoise3 06.Unique/all.sort.fa -zotus 07.OTU/unoise/zotus.fa -tabbedout 07.OTU/unoise/unoise3.txt
