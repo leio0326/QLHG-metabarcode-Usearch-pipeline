71350cc32ab915ef3e0914ebb97244a5d49ebba0
leio0326 <136607704+leio0326@users.noreply.github.com>
2025-10-16 15:03:40 +0800

    Update 04.Maxee.sh

--- Userach pipeline/04.Maxee.sh
+++ Userach pipeline/04.Maxee.sh
@@ -1,21 +1,13 @@
+# Quality control of merged reads
 #!/bin/bash
-cd /data01/duanlei/ecology/rawdata/COI/20250115/
+cd "your working path"
+USEARCH_PATH="~/soft/usearch"
 
-# 指定 usearch 的路径
-USEARCH_PATH="/public/home/wangwen_lab/duanlei/soft/usearch"
-
-# 创建输出文件夹（如果不存在）
-OUTPUT_DIR_FILTERED="04.Maxee"
+OUTPUT_DIR="04.Maxee"
 mkdir -p "$OUTPUT_DIR_FILTERED"
 
-# 遍历合并后的 FASTQ 文件
 for merged_file in 03.Merged/*_merged.fq; do
-    # 生成输出过滤后的文件名，转换为 .fasta 格式
-    filtered_file="$OUTPUT_DIR_FILTERED/$(basename "${merged_file/_merged.fq/.MAXEE_1.fasta}")"
-    
-    # 使用 usearch 进行过滤并转换为 FASTA 格式
+    filtered_file="$OUTPUT_DIR/$(basename "${merged_file/_merged.fq/.MAXEE_1.fasta}")"
     "$USEARCH_PATH" -fastq_filter "$merged_file" -fastaout "$filtered_file" --fastq_maxee 1
-    
     echo "Filtered and converted: $merged_file -> $filtered_file"
 done
-
