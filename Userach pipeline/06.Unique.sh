2e791d8d7d14ef45cb262d78e87d8636468550f9
leio0326 <136607704+leio0326@users.noreply.github.com>
2025-10-16 15:14:10 +0800

    Update 06.Unique.sh

--- Userach pipeline/06.Unique.sh
+++ Userach pipeline/06.Unique.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /data01/duanlei/ecology/rawdata/COI/20250115/05.length
-
-for input_file1 in *.fa; do
-    
-    file_prefix="${input_file1%.fa}"
-     
-    usearch -fastx_uniques $input_file1 -fastaout ../06.Unique/${file_prefix}.uni.fa -sizeout 
-
+cd "your working path"
+USEARCH_PATH="~/soft/usearch"
+INPUT_DIR="05.length"
+OUTPUT_DIR="06.Unique"
+mkdir -p "$OUTPUT_DIR"
+for input_file1 in "$INPUT_DIR"/*.fa; do
+    file_prefix="${input_file1%.fa}"     
+    "$USEARCH_PATH"  -fastx_uniques $input_file1 -fastaout "$OUTPUT_DIR"/${file_prefix}.uni.fa -sizeout 
 done
 
